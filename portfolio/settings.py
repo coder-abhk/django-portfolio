d7e52fe0527202ddd1f961e6974b07297a297d32
abhk07 <abhkrwt07@gmail.com>
2020-12-18 23:04:31 +0530

    hostname

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '$8*+(cj*$*eh6s)bd=&ovnz&4louebt9f7px@%n(oie*e$4tq#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abhishekrawat.herokuapp.com']
 
 
 # Application definition
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
